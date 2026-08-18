48b07b7e5aefa0d8800c806d47caa7bed442837b
arvinbehbahani562@gmail.com <arvinbehbahani562@gmail.com>
2026-08-18 21:17:43 +0330

    used </Link> element instead of the </a> to make the side single page application (SPA)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from "react";
 
+import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import ChechoutPage from "./components/CheckoutPage";
 import OrdersPage from "./components/OrdersPage";
@@ -13,10 +14,31 @@ function App() {
   return (
     <>
       <Routes>
-        <Route index element={<HomePage />} />
+        <Route
+          index
+          element={
+            <>
+              <Header /> <HomePage />
+            </>
+          }
+        />
         <Route path="checkout" element={<ChechoutPage />} />
-        <Route path="orders" element={<OrdersPage />}></Route>
-        <Route path="tracking" element={<TrackingPage />} />
+        <Route
+          path="orders"
+          element={
+            <>
+              <Header /> <OrdersPage />
+            </>
+          }
+        ></Route>
+        <Route
+          path="tracking"
+          element={
+            <>
+              <Header /> <TrackingPage />
+            </>
+          }
+        />
       </Routes>
     </>
   );
