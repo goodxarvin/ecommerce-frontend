1cb22ced180abd22da6e49e191fab4fde67ac0cc
arvinbehbahani562@gmail.com <arvinbehbahani562@gmail.com>
2026-08-18 19:48:33 +0330

    added the index and the checkout routes to the project with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 // import { useState } from "react";
 
 import HomePage from "./components/HomePage";
+import { Routes, Route } from "react-router";
 import "./App.css";
 
+// Route is basically a page with a given path in the url from main path to be shown.
+//shortcut: instead of path="/" you can write "index" as well
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<HomePage />} />
+      </Routes>
     </>
   );
 }
